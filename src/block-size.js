d8e3b65a67466023c2317435c64cfcb27003ec5b
KM <the.real.laplace@gmail.com>
2020-11-18 22:24:27 +0200

    chore(commit): force

--- src/block-size.js
+++ src/block-size.js
@@ -1,10 +1,15 @@
-const Fsp = require('fs').promises
+const Fs = require('fs')
+const Fsp = Fs.promises
 const Path = require('path')
 const Os = require('os')
 
 const TMP = Path.join(Os.tmpdir(), 'get-block-size-')
 
+let cached = null
+
 const getBlockSize = async () => {
+	if (cached) { return cached }
+
 	const temp_dir = await Fsp.mkdtemp(TMP)
 	const file_path = Path.join(temp_dir, 'dummy')
 	await Fsp.appendFile(file_path, 'a')
@@ -13,7 +18,26 @@ const getBlockSize = async () => {
 
 	await Fsp.rm(temp_dir, { recursive: true })
 
-	return stats.blksize
+	cached = stats.blksize
+	return cached
 }
 
-module.exports = { getBlockSize }
+const getBlockSizeSync = () => {
+	if (cached) { return cached }
+
+	const temp_dir = Fs.mkdtempSync(TMP)
+	const file_path = Path.join(temp_dir, 'dummy')
+	Fs.appendFileSync(file_path, 'a')
+
+	const stats = Fs.statSync(file_path)
+
+	Fs.rmSync(temp_dir, { recursive: true })
+
+	cached = stats.blksize
+	return cached
+}
+
+module.exports = {
+	getBlockSize,
+	getBlockSizeSync,
+}
